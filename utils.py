b757adcfbb3b95832f4d28cf8a0096ffcf971a37
Trevor Moyer <trevorm4@illinois.edu>
2020-04-20 08:37:53 -0400

    added utils

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 import discord
-
-def get_member_name(member):
+def get_member_name(member, lower=True):
+    if type(member) == str:
+        if lower:
+            return member.lower()
+        return member
     if member.nick:
+        if lower:
+            return member.nick.lower()
         return member.nick
+    if lower:
+        return member.name.lower()
     return member.name
