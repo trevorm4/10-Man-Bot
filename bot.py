fad183ae8a82966ed35a5b1d7c337896cfe52c80
Trevor Moyer <trevorm4@illinois.edu>
2020-04-20 09:12:03 -0400

    Got rid of name dictionary due to enforced typing

--- bot.py
+++ bot.py
@@ -6,24 +6,23 @@ from utils import get_member_name
 from converters import Player
 
 class Bot(commands.Bot):
-    def __init__(self,command_prefix):
+    def __init__(self,command_prefix, drafting_scheme):
         commands.Bot.__init__(self,command_prefix=command_prefix)
         self.teams = {"A" : [], "B" : []}
-        self.ready_dict = {}
-        self.nick_to_player = {}
         self.ready_dict = {}
         self.remaining = []
         self.captains = {"A" : None, "B": None}
         self.channels = {"A" : None, "B" : None}
         self.order = []
+        self.drafting_dict = {"A" : [int(s[1]) for s in drafting_scheme if s[0] == "A"],
+                              "B" : [int(s[1]) for s in drafting_scheme if s[0] == "B"]}
+        self.turn = -1
     async def set_captain(self, cap : Player, team):
         """
         Sets captain for specified team
             :param cap: discord.Member object representing user that is now captain
             :param team: string for team name ("A" or "B")
         """
-        if type(cap) == str:
-            cap = self.nick_to_player[get_member_name(cap)]
         self.captains[team.upper()] = cap
         await self.add_to_team(cap,team)
         self.order.append(cap)
@@ -34,8 +33,6 @@ class Bot(commands.Bot):
             :param team: string representing team name
             :return discord.Embed: embed message to display
         """
-        if get_member_name(player) not in self.nick_to_player.keys():
-            self.nick_to_player[get_member_name(player)] = player
         if player in self.remaining:
             self.teams[team].append(player)
             self.remaining.remove(player)
@@ -46,6 +43,7 @@ class Bot(commands.Bot):
         """
         Clears instance variables in preperation for new game
             :param players: list of Discord.Member variables representing players
+            :param drafting_scheme: list contains strings of format "<A/B><1-2>" where first char is team and 2nd char is # players
         """   
         if len(players) != 10:
             return discord.Embed(title="Valorant 10 Man Bot",
@@ -53,8 +51,8 @@ class Bot(commands.Bot):
         self.teams = {"A": [], "B" : []}
         self.captains = {"A" : None, "B" : None}
         self.nick_to_player = {get_member_name(p) : p for p in players}
-        self.ready_dict = {p : False for p in players}
         self.remaining = players.copy()
+        self.turn = 1
         self.order = []
         return discord.Embed(title="Valorant 10 Man Bot",
             description="New game started".format(len(players)))
@@ -69,7 +67,6 @@ class Bot(commands.Bot):
         caps = random.sample(self.remaining, 2) # 2 captains
         for i,team in enumerate(self.captains.keys()):
             await self.set_captain(caps[i],team)
-            #self.remaining.remove(caps[i])
         return discord.Embed(title="Valorant 10 Man Bot",
             description="The captains are @{} (1st pick) and @{} (2nd pick)".format(get_member_name(caps[0],lower=False),get_member_name(caps[1],lower=False)))
     async def get_remaining(self):
@@ -78,14 +75,6 @@ class Bot(commands.Bot):
         :ret: list of remaining players of type Discord.Member
         """   
         return self.remaining
-    async def ready_up(self, player : Player):
-        """
-        Sets status of player to ready
-            :param player: discord.Member object representing player
-            :return discord.Embed: embed object to display
-        """   
-        self.ready_dict[player] = True
-        return discord.Embed(title="Valorant 10 Man Bot",description="{} is now ready".format(get_member_name(player)))
     async def move_player(self, player : Player, channel):
         """
         Moves player to specified channel
@@ -99,23 +88,26 @@ class Bot(commands.Bot):
             :param captain: discord.Member object representing captain
             :param player:  discord.Member object representing player
         """
-        if (type(player) == str and get_member_name(player) not in self.nick_to_player.keys()) \
-            or (type(player) == discord.Member and player not in self.nick_to_player.values()):
+        if player not in self.nick_to_player.values():
             return discord.Embed(title="Valorant 10 Man Bot",description="{} is not a valid player".format(player))
         elif captain not in self.captains.values():
             return discord.Embed(title="Valorant 10 Man Bot",description="{} is not a captain".format(get_member_name(captain)))
             
-        player_obj = self.nick_to_player[get_member_name(player)] if type(player) == str else player
         team = "A" if captain == self.captains["A"] else "B"
         opp = "B" if team == "A" else "A"
 
-        if len(self.teams[team]) == 0 and len(self.teams[opp]) == 0 and self.order[0] != captain:
+        if len(self.teams[team]) == 0 and len(self.teams[opp]) == 0 and self.order[0] != captain: 
             return discord.Embed(title="Valorant 10 Man bot",description="The other captain gets to draft first, sorry!")
-        elif len(self.teams[team]) > len(self.teams[opp]):
+        elif len(self.teams[team]) > len(self.teams[opp]) \
+                or len(self.teams[team]) >= sum(self.drafting_dict[team][:self.turn]) + 1: # + 1 to include captain
             return discord.Embed(title="Valorant 10 Man bot",description="You've already drafted this turn, please wait for the other captain")
-
-        embed = await self.add_to_team(player_obj, team)
+        
+        embed = await self.add_to_team(player, team)
         channel = channel_dict[team]
-        await self.move_player(player_obj,channel)
+        await self.move_player(player,channel)
+
+        if team == "B" and len(self.teams[team]) == sum(self.drafting_dict[team][:self.turn]) + 1: # + 1 to include captain
+            self.turn += 1
+
         return embed
         
